651b41f8a65cdec559a22c3e3a45a8a5f3cdb17e
mabhay1197 <76745426+mabhay1197@users.noreply.github.com>
2021-01-29 13:07:39 +0530

    Add files via upload

---
+++ Fibonacci Series WithRecursion AndDynamic Programming.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+#define NIL 0
+#define MAX 100 
+
+int lookup[MAX];
+
+using namespace std;
+
+void initialize()
+{
+	int i;
+	for(i=0;i<MAX;i++)
+	{
+		lookup[i]=NIL;
+	}
+}
+
+int fib(int num)
+{
+    if(lookup[num]==0)
+	{
+		if(num<=1)
+		{
+			lookup[num] = num;	
+		}
+		else
+		{
+			lookup[num] = fib(num-1)+fib(num-2);	
+		}	
+	}
+	return lookup[num];	
+}
+
+int main()
+{
+	int num;
+	cout <<"Enter the limit for fibonacci series : " <<endl;
+	cin>> num;
+	long double series = fib(num);
+	cout<<"The value of Fibonacci series for limit " << num << " is " << series ;
+	
+	return 0;
+}
